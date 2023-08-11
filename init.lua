35e1eaa46fb27ef13bd1c904df0349cbc07fd34f
tacotexmex <tacotexmex@protonmail.com>
2023-08-11 16:24:24 +0200

    Initial commit

--- init.lua
+++ init.lua
@@ -1,37 +1,12 @@
 math.randomseed(os.time())
 
+local charge_exists = minetest.get_modpath("charge")
+
 -- Variate sound pitch ever so slightly
 function vpitch(variations)
 	local variation = variations - 2
 	return (100 - (math.random(-variation, variation)) * 10) / 100
 end
-
-local charge = {}
-
-minetest.register_on_joinplayer(function(player)
-	local name = player:get_player_name()
-	charge[name] = 0
-end)
-
-minetest.register_on_leaveplayer(function(player)
-	local name = player:get_player_name()
-	charge[name] = nil
-end)
-
-controls.register_on_hold(function(player, control_name, time)
-	if control_name == "sneak" then
-		local name = player:get_player_name()
-		charge[name] = charge[name] + time
-		minetest.chat_send_all(charge[name])
-	end
-end)
-
-controls.register_on_release(function(player, control_name, time)
-	if control_name == "sneak" then
-		local name = player:get_player_name()
-		charge[name] = 0
-	end
-end)
 
 -- Redefine drop function (Thanks jordan4ibenez!)
 -- Throw items using player's velocity
@@ -50,17 +25,23 @@ function minetest.item_drop(itemstack, dropper, pos, sound)
 		local item = itemstack:to_string()
 		local obj = minetest.add_item(pos, item)
 		sound = sound or "throw"
-		if charge[name] > 3 then
-			charge[name] = 0
-		end
 		if obj then
-			vel.x = (dir.x * (5 + (charge[name] * 10))) + vel.x
-			vel.y = (dir.y * (5 + (charge[name] * 10))) + 2 + vel.y
-			vel.z = (dir.z * (5 + (charge[name] * 10))) + vel.z
+			local extra_vel = 0
+			if charge_exists then
+				if charge.amount[name] then
+					extra_vel = charge.amount[name] * 5
+				end
+				charge.amount[name] = 0
+				if charge.sound[name] then
+					minetest.sound_fade(charge.sound[name], 10, 0)
+				end
+			end
+			vel.x = (dir.x * (4 + extra_vel)) + vel.x
+			vel.y = (dir.y * (4 + extra_vel)) + 2 + vel.y
+			vel.z = (dir.z * (4 + extra_vel)) + vel.z
 			obj:add_velocity(vel)
 			obj:get_luaentity().dropped_by = dropper:get_player_name()
 			itemstack:clear()
-			charge[name] = 0
 			minetest.sound_play(sound, {
 				pos = pos,
 				gain = 0.1,
